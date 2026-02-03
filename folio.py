09889c03b9449b0793da6d9b2b1bfb493d7c72b3
AlffousseyniDiakhite <seny1kaba10@gmail.com>
2026-02-03 17:04:53 +0000

    Add files via upload

---
+++ folio.py
@@ -0,0 +1,108 @@
+import streamlit as st
+
+
+st.title (" ALFOUSSEYNI DIAKHITE ")
+
+st.subheader(" Technicien supérieur en Géomatique ")
+
+
+with st.sidebar:
+    st.image("1015.jpeg")
+    
+    
+
+    st.markdown("📍 Adresse  ")
+    
+    st.write(""" Keur MBAYE fall,  Dakar,  Senegal """)
+    
+            
+    st.header("""📞Contact""")
+        
+    st.write("""Email:Seny1kaba10@gmail.com""")
+        
+    st.write("Telephone:77 750 01 88")
+        
+    st.header("""Education""")
+        
+    st.markdown(("BTS Géomatique en cours"))
+        
+    st.write("Baccalauriat L2 2024" )
+        
+  
+    
+   
+  
+
+     
+    
+st.header("  Profil ")
+st.write("""
+- Techniciens superieur en geomatique un fort intêret pour "" satellittes"" et l'observation de la terre 
+Le traitement des données avec une 
+La sécurité des donnés géopatiales. """)
+
+
+
+
+
+st.header("🧠Compétence Techniques")
+st.write ("""  
+* ArcGis""")
+
+st.write(""" 
+* QGIS""")
+
+st.write("""
+* AutoCad""")
+
+
+st.write("""
+* Sketchup""")
+
+st.write("""
+* 🧑‍💻Pogrammation de python """)
+
+
+st.write("""
+* Matériel bureautique""")
+
+
+st.header("""✅ Projets""")
+
+
+
+st.write("""
+
+* Projets de carthographie collécte de données (G15) (Masalikoun)  """)
+
+st.write("""
+
+* Dessin de plan 2D sur Autocad """)
+
+st.write("""
+
+* Dessin de plan 3D sur Sketchup""")
+
+
+st.write("""
+
+* Geographie Réalisation  de Carte sur ArcGis la région de Louga """)
+
+st.write("""
+* Numerisation du quartier de Grand Yoff sur QGIS """)
+
+st.write("""
+* Réalisation d'une application de calcul sur programmation de python""")
+
+
+
+
+
+
+
+
+
+
+
+
+
